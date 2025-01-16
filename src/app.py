6f94ccead4376c9f31b1d743fc0e335913b2f05e
Tom <tom.wei@onedegree.hk>
2025-01-16 14:40:07 +0800

    [BE] Initialize the repo

---
+++ src/app.py
@@ -0,0 +1,9 @@
+from submodule.utils.util import GeneralUtil
+
+# Example usage of GeneralUtil
+def main():
+    result = GeneralUtil.get_current_time()
+    print(result)
+
+if __name__ == "__main__":
+    main()
