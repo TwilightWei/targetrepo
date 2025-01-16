a834d51846367d3742ae0c40f91acfe0094436f4
Tom <tom.wei@onedegree.hk>
2025-01-16 15:09:50 +0800

    [BE] Add submodule

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
 from submodule.utils.util import GeneralUtil
 
+
 # Example usage of GeneralUtil
 def main():
     result = GeneralUtil.get_current_time()
     print(result)
+
 
 if __name__ == "__main__":
     main()
